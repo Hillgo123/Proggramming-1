3c47a586367473dff27bf7a39cf1bd98259e6a51
Leo Bergqvist <leo@bergqvist.se>
2023-01-31 16:21:42 +0100

    31-1 start of java

---
+++ 31-1/main.java
@@ -0,0 +1,8 @@
+class Main {
+    int x = (int) (Math.random() * 100);
+
+    public static void main(String[] args) {
+        Main my_obj = new Main();
+        System.out.println(my_obj.x);
+    }
+}
